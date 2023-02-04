26fc4f1f78763c946e214308dd0b04034ae2491f
zocasa <krishnaarpit95@gmail.com>
2023-02-04 01:46:40 -0800

    TO BE REVERTED

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import plotgraph
 if __name__ == "__main__":
     filename = filehandler.get_filename()
     file = filehandler.open_read_file(filename, True)
-    fig, subplot = plotgraph.create_graph(file)
+    fig, subplot = plotgraph.create_graph(file, True)
