b112bc2cce503d5b05092d3b73b35e34f3c8820a
zocasa <krishnaarpit95@gmail.com>
2023-02-12 16:39:35 -0800

    Fix many edge-cases in gitpusher, mainly concerning commands which return exit code 0 even when they do not do what I wanted, such as 'git add .' returning exit code 0 even when nothing is added. Fix many stupidities and made little readable. UNTESTED

--- githelper.py
+++ githelper.py
@@ -1,13 +1,41 @@
 import re
 import time
-from enum import StrEnum
+from enum import Enum
 
 import subprocess
 
 active_projects_config_filename = ''
+active_projects_config_data_delimiter = '::'
 
 
-class ProjectType(StrEnum):
+class Git:
+    IS_VALID_DIRECTORY = 'git rev-parse --is-inside-work-tree'
+    HAS_COMMITS = 'git rev-list --count origin/main..@'
+    REMOTE_BRANCHES = 'git branch -r'
+
+    ADD_ALL = 'git add .'
+    RESTORE_STAGED = 'git restore --staged .'
+    REBASE = 'git pull --rebase'
+    COMMIT = 'git commit -m'
+    SOFT_RESET_1 = 'git reset --soft HEAD~1'
+    PUSH = 'git push origin'
+    DELETE_REMOTE = 'git push origin -d'
+
+    SAVE_STASH = 'git stash push -m'
+    APPLY_STASH = 'git stash apply'
+    DROP_STASH = 'git stash drop'
+
+    STASH_SAVE_NO_OP_MESSAGE = 'No local changes to save'
+
+    PREFIX_MESSAGE = 'GitHelper_'
+    STAGED_MESSAGE = PREFIX_MESSAGE + 'staged_changes'
+    UNSTAGED_MESSAGE = PREFIX_MESSAGE + 'unstaged_changes'
+
+    PREFIX_HEAD = 'HEAD'
+    BRANCH_NAME = 'backup_branch'
+
+
+class ProjectType(Enum):
     # The directory is the git package and is private (no collaboration; can be rebased which is obviously superior)
     DEFAULT = 'default'
     # The directory is work related java workspace
@@ -15,45 +43,51 @@ class ProjectType(StrEnum):
 
 
 def get_active_project_details():
-    # use with thingy
+    # TODO use 'with' thingy
     active_project_file = open(active_projects_config_filename, 'r')
     active_project_paths = []
     active_project_types = []
 
     for active_project_detail in active_project_file:
-        active_project_data = active_project_detail.split('::')
+        active_project_data = active_project_detail.split(active_projects_config_data_delimiter)
         if len(active_project_data) == 2:
             active_project_paths.append(active_project_data[0])
             active_project_types.append(active_project_data[1])
 
     return active_project_types, active_project_paths
-    # return ['default'], ['']
 
 
 def handle_active_projects(function):
     active_project_details = zip(*get_active_project_details())
     for active_project_type, active_project_path in active_project_details:
-        process = subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'])
-        if process.returncode == 0:
-            if active_project_type.lower() == ProjectType.DEFAULT:
-                process = subprocess.run(['cd', active_project_path])
-                if process.returncode == 0:
+        successful, _ = run_command(Git.IS_VALID_DIRECTORY)
+        if successful:
+            if active_project_type.lower() == ProjectType.DEFAULT.value:
+                successful, _ = run_command('cd', active_project_path)
+                if successful:
                     function()
 
 
 def rebase_project():
-    subprocess.run(['git', 'stash', 'push', '-m', '[logOffAutoStasher] staged changes'])
-    subprocess.run('git add .')
-    subprocess.run(['git', 'stash', 'push', '-m', '[logOffAutoStasher] unstaged changes'])
-    process = subprocess.run(['git', 'pull', '--rebase'])
-    if process.returncode == 0:
-        process = subprocess.run(['git', 'stash', 'apply'])
-        if process.returncode == 0:
-            subprocess.run(['git', 'restore', '--staged', '.'])
-            subprocess.run(['git', 'stash', 'drop'])
-            process = subprocess.run(['git', 'stash', 'apply'])
-            if process.returncode == 0:
-                subprocess.run(['git', 'stash', 'drop'])
+    staged_changes_present = stash(Git.STAGED_MESSAGE)
+
+    run_command(Git.ADD_ALL)
+    unstaged_changes_present = stash(Git.UNSTAGED_MESSAGE)
+
+    commits_present = has_commits()
+    successful, _ = run_command(Git.REBASE)
+    if not commits_present or successful:
+        if unstaged_changes_present:
+            successful, _ = run_command(Git.APPLY_STASH)
+            if successful:
+                run_command(Git.RESTORE_STAGED)
+                run_command(Git.DROP_STASH)
+            else:
+                return
+
+        if staged_changes_present:
+            run_command(Git.APPLY_STASH)
+            run_command(Git.DROP_STASH)
 
 
 def rebase_projects():
@@ -62,40 +96,76 @@ def rebase_projects():
 
 def push_local_work():
     timestamp = str(int(time.time()))
-    process1 = subprocess.run(['git', 'push', 'origin', 'HEAD:logOffAutoPusher_committedChanges_', timestamp])
-    subprocess.run(['git', 'commit', '-m', '[logOffAutoPusher] staged changes'])
-    process2 = subprocess.run(['git', 'push', 'origin', 'HEAD:logOffAutoPusher_stagedChanges_', timestamp])
-    subprocess.run(['git', 'add', '.'])
-    subprocess.run(['git', 'commit', '-m', '[logOffAutoPusher] unstaged changes'])
-    process3 = subprocess.run(['git', 'push', 'origin', 'HEAD:logOffAutoPusher_unstagedChanges_', timestamp])
-    if process3.returncode == 0:
-        subprocess.run(['git', 'reset', '--soft', 'HEAD~1'])
-        subprocess.run(['git', 'restore', '--staged', '.'])
-        if process2.returncode == 0:
-            subprocess.run(['git', 'reset', '--soft', 'HEAD~1'])
 
-    if process1.returncode == 0 and process2.returncode == 0 and process3.returncode == 0:
-        cleanup_remote(timestamp)
+    commits_present = has_commits()
+
+    staged_changes_present = run_command(Git.COMMIT, Git.STAGED_MESSAGE)
+
+    run_command(Git.ADD_ALL)
+    unstaged_changes_present = run_command(Git.COMMIT, Git.UNSTAGED_MESSAGE)
+
+    pushed_changes = False
+    if commits_present or staged_changes_present or unstaged_changes_present:
+        pushed_changes = run_command(Git.PUSH,
+                                     Git.PREFIX_HEAD + ':' + Git.PREFIX_MESSAGE + Git.BRANCH_NAME + '_' + timestamp)
+
+    if unstaged_changes_present:
+        run_command(Git.SOFT_RESET_1)
+        run_command(Git.RESTORE_STAGED)
+
+    if staged_changes_present:
+        run_command(Git.SOFT_RESET_1)
+
+    # TODO This will keep moving the remote commit even if it is not needed instead of being efficient
+    if pushed_changes:
+        cleanup_remote_branches(timestamp)
 
 
 def push_local_works():
     handle_active_projects(push_local_work)
 
 
-def cleanup_remote(timestamp):
-    process = subprocess.run(['git', 'branch', '-r'], capture_output=True, text=True)
-    if process.returncode == 0:
-        output = process.stdout.splitlines()
+def cleanup_remote_branches(timestamp='0', delete_all=False):
+    if not delete_all:
+        if timestamp == '0':
+            return
+
+    successful, output = run_command(Git.REMOTE_BRANCHES)
+    if successful:
         for remote_branch_name in output:
-            if 'origin/main' not in remote_branch_name:
-                pattern = re.compile(r'^\s+origin/(logOffAutoPusher_(committed|staged|unstaged)Changes_(\d+))$')
+            remote_branch_name = remote_branch_name.strip()
+            if 'origin/main' != remote_branch_name:
+                pattern = re.compile(r'^origin/(GitHelper_backup_branch_(\d+))$')
                 match = pattern.match(remote_branch_name)
                 if match:
-                    remote_branch_timestamp = int(match.group(3))
+                    remote_branch_timestamp = int(match.group(2))
                     latest_timestamp = int(timestamp)
-                    if remote_branch_timestamp < latest_timestamp:
-                        subprocess.run(['git', 'push', 'origin', '-d', match.group(1)])
+                    if delete_all or remote_branch_timestamp < latest_timestamp:
+                        run_command(Git.DELETE_REMOTE, match.group(1))
+
+
+def wrapup_remote_branches():
+    cleanup_remote_branches(delete_all=True)
+
+
+def stash(message):
+    _, output = run_command(Git.SAVE_STASH, message)
+    return Git.STASH_SAVE_NO_OP_MESSAGE not in output
+
+
+def has_commits():
+    successful, output = run_command(Git.HAS_COMMITS)
+    if not successful:
+        raise Exception('Could not successfully run command to figure out if local repository has commits')
+    return int(output[0]) > 0
+
+
+def run_command(command, args=None):
+    if args is None:
+        args = []
+    process = subprocess.run([*command.split(), *args], capture_output=True, text=True)
+    return process.returncode == 0, process.stdout.splitlines()
 
 
 if __name__ == "__main__":
-    cleanup_remote('1676224409')
+    cleanup_remote_branches('1676224409')
